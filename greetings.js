2ed89b1a4113578c8f1d478c6c6cdf2816216e2f
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2024-03-09 17:47:06 -0600

    added emoji

--- greetings.js
+++ greetings.js
@@ -3,6 +3,21 @@
       const userInputField = document.getElementById("userInput");
       const button2 = document.getElementById("button1");
 
+      // const nameListBox = document.querySelector(".names");
+
+      // const names = [ 'hardelz' , 'emma' , 'pablo' , 'precious' ]
+
+      // userInputField.addEventListener('input' , e => {
+
+      //     const filteredNames = names.filter(name => name.toLowerCase().includes(userInputField.value.toLowerCase()))
+
+      //     nameListBox.innerHTML = ''
+
+      //     filteredNames.forEach(name => {
+      //       nameListBox.innerHTML += `<h1>${name}</h1>`
+      //     })
+      // })
+
       button2.addEventListener("click", () => {
         const user = userInputField.value;
 
@@ -10,7 +25,7 @@
 
         if (user == "") {
           const h1Element = document.querySelector("h1");
-          h1Element.innerHTML = "pls insert your fucking name pls";
+          h1Element.innerHTML = "pls insert your fucking name pls 😡";
           h1Element.style.color = "red";
           isInputIncorrect = true;
           return;
@@ -18,7 +33,7 @@
 
         if (!isNaN(user)) {
           const h1Set = document.querySelector("h1");
-          h1Set.innerHTML = "Please insert alphabets";
+          h1Set.innerHTML = "Please insert alphabets 😤";
           h1Set.style.color = "red";
           isInputIncorrect = true;
           return;
@@ -26,12 +41,12 @@
 
         if (isInputIncorrect) {
           const h1Element = document.querySelector("h1");
-          h1Element.innerHTML = `Thank you very much ${user}`;
+          h1Element.innerHTML = `Thank you very much ${user} ❤`;
           h1Element.style.color = "black";
           return;
 
         }
 
-        document.querySelector("h1").innerHTML = `Thank you very much ${user}`;
+        document.querySelector("h1").innerHTML = `Thank you very much ${user} ❤`;
       });
    
